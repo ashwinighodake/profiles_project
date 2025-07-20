2e63cbab0e24d953fdb25efd11829449a39250e0
Ashwini Ghodake <ashwini752000@gmail.com>
2025-07-20 16:11:20 +0530

    "Configured for deployment"

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,44 +3,38 @@
 set -e
 
 # TODO: Set to URL of git repo.
-PROJECT_GIT_URL='git@github.com:ashwinighodake/Django-project.git'
+PROJECT_GIT_URL='https://github.com/ashwinighodake/profiles_project.git'
 
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
 # Set Ubuntu Language
-sudo locale-gen en_GB.UTF-8
+locale-gen en_GB.UTF-8
 
 # Install Python, SQLite and pip
-
-# Install Python, SQLite, pip, and other dependencies
 echo "Installing dependencies..."
-sudo apt-get update
-sudo apt-get install -y python3-dev python3-venv sqlite3 python3-pip supervisor nginx git
+apt-get update
+apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
 
-# Create the app directory
-sudo mkdir -p $PROJECT_BASE_PATH
-sudo chown $USER:$USER $PROJECT_BASE_PATH
-
+mkdir -p $PROJECT_BASE_PATH
 git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
-cd $PROJECT_BASE_PATH/profiles_project
 
 python3 -m venv $PROJECT_BASE_PATH/env
 
-$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/profiles_project/requirements.txt
+$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt uwsgi
 
 # Run migrations
-$PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/profiles_project/manage.py migrate
+$PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py migrate
 
 # Setup Supervisor to run our uwsgi process.
-sudo cp $PROJECT_BASE_PATH/profiles_project/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
-sudo supervisorctl reread
-sudo supervisorctl update
-sudo supervisorctl restart profiles_api
+cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
+supervisorctl reread
+supervisorctl update
+supervisorctl restart profiles_api
 
 # Setup nginx to make our application accessible.
-sudo cp $PROJECT_BASE_PATH/profiles_project/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
-sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
-sudo systemctl restart nginx.service
+cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
+rm /etc/nginx/sites-enabled/default
+ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
+systemctl restart nginx.service
 
 echo "DONE! :)"
